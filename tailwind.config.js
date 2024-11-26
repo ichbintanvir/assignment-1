a93832abf831d6058b78ee5aef04c40f33261665
ichbintanvir <tanvirnoyon.bubt@gmail.com>
2024-11-26 23:18:13 +0600

    push the assignment full code

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'plex-sans': ['"IBM Plex Sans"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
